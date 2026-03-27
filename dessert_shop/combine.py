51f497847e998160a265faa399cb8b4dacae4cd8
wpixton <william@thepixtons.com>
2026-03-27 16:30:01 -0600

    Delete dessert_shop directory

--- dessert_shop/combine.py
+++
@@ -1,8 +0,0 @@
-from typing import Protocol, runtime_checkable
-@runtime_checkable
-class Combinable(Protocol):
-    def can_combine(self, other: "Combinable") -> bool:
-        ...
-
-    def combine(self, other: "Combinable") -> "Combinable":
-        ...
--- dessert_shop/packaging.py
+++
@@ -1,4 +0,0 @@
-from typing import Protocol
-class Packaging(Protocol):
-    packaging: str
-    
